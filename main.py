0b826fd780f87117aabf0a5b873ae60d61c91d5a
j.sha <jsha1989@gmail.com>
2023-07-11 21:51:54 +1000

    Fixed bug where character request returns wrong info because there is another character with the same name.

--- main.py
+++ main.py
@@ -31,19 +31,22 @@ def get_book(book_id):
     # reformat date
     release = data["released"].split("T")
     release_date = release[0]
-    format = '%Y-%m-%d'
-    release_obj = datetime.strptime(release_date, format)
+    date_format = '%Y-%m-%d'
+    release_obj = datetime.strptime(release_date, date_format)
     release_str = release_obj.strftime('%d %B, %Y')
     # get list of POV chars
     pov_chars = data["povCharacters"]
-    pov_chars_list = []
+    pov_chars_dict = {}
     for char in pov_chars:
         char_response = requests.get(char)
         char_data = char_response.json()
         char_name = char_data["name"]
-        pov_chars_list.append(char_name)
-    return render_template("book.html", title=title, pages=pages, release=release_str, pov_chars=pov_chars_list,
+        char_split = char.split("/")
+        char_id = char_split[5]
+        pov_chars_dict[char_id] = char_name
+    return render_template("book.html", title=title, pages=pages, release=release_str, pov_chars=pov_chars_dict,
                            book_id=book_id)
+
 
 # display house info
 @app.route("/house/<house_id>")
@@ -58,52 +61,65 @@ def get_house(house_id):
     house_weapons = data["ancestralWeapons"]
     # get list of members
     house_members = data["swornMembers"]
-    house_members_list = []
+    house_members_dict = {}
     for member in house_members:
         member_response = requests.get(member)
         member_data = member_response.json()
         member_name = member_data["name"]
-        house_members_list.append(member_name)
+        member_split = member.split("/")
+        member_id = member_split[5]
+        house_members_dict[member_id] = member_name
     return render_template("house.html", house_name=house_name, house_region=house_region,
                            house_words=house_words, house_titles=house_titles, house_seats=house_seats,
-                           house_weapons=house_weapons, house_members=house_members_list, house_id=house_id)
+                           house_weapons=house_weapons, house_members=house_members_dict, house_id=house_id)
 
 
-#display character info
-@app.route("/character/<char>")
-def get_char(char):
-    response = requests.get(URL + f"/characters?name={char}")
+# display character info
+@app.route("/character/<char_id>")
+def get_char(char_id):
+    response = requests.get(URL + f"/characters/{char_id}")
     data = response.json()
-    char_name = data[0]["name"]
-    char_gender = data[0]["gender"]
-    char_culture = data[0]["culture"]
-    char_born = data[0]["born"]
-    char_died = data[0]["died"]
-    char_titles = data[0]["titles"]
-    char_aliases = data[0]["aliases"]
+    char_name = data["name"]
+    char_gender = data["gender"]
+    char_culture = data["culture"]
+    char_born = data["born"]
+    char_died = data["died"]
+    char_titles = data["titles"]
+    char_aliases = data["aliases"]
     # get spouse name
-    char_spouse = data[0]["spouse"]
+    char_spouse = data["spouse"]
+    spouse_dict = {}
     if char_spouse != "":
-        char_spouse = get_name_from_api(char_spouse)
+        spouse_name = get_name_from_api(char_spouse)
+        spouse_split = char_spouse.split("/")
+        spouse_id = spouse_split[5]
+        spouse_dict[spouse_id] = spouse_name
     # get allegiance
-    char_allegiance = data[0]["allegiances"]
-    char_allegiance_list = []
+    char_allegiance = data["allegiances"]
+    allegiance_dict = {}
     for allegiance in char_allegiance:
-        char_allegiance_name = get_name_from_api(allegiance)
-        char_allegiance_list.append(char_allegiance_name)
+        allegiance_name = get_name_from_api(allegiance)
+        allegiance_split = allegiance.split("/")
+        allegiance_id = allegiance_split[5]
+        allegiance_dict[allegiance_id] = allegiance_name
     # get books
-    char_books = data[0]["books"]
-    char_pov_books = data[0]["povBooks"]
-    char_books_list = []
+    char_books = data["books"]
+    char_pov_books = data["povBooks"]
+    char_books_dict = {}
     for book in char_books:
         book_name = get_name_from_api(book)
-        char_books_list.append(book_name)
+        book_split = book.split("/")
+        book_id = book_split[5]
+        char_books_dict[book_id] = book_name
     for book in char_pov_books:
         book_name = get_name_from_api(book)
-        char_books_list.append(book_name + " (POV character)")
-    char_actor = data[0]["playedBy"]
+        book_split = book.split("/")
+        book_id = book_split[5]
+        char_books_dict[book_id] = book_name + " (POV character)"
+    char_actor = data["playedBy"]
+    char_actor_string = ', '.join(char_actor)
     # get google image of character
-    url = 'https://www.google.com/search?q={0}&tbm=isch'.format(char_name + "game of thrones")
+    url = 'https://www.google.com/search?q={0}&tbm=isch'.format(char_name + " game of thrones")
     content = requests.get(url).content
     soup = BeautifulSoup(content, 'html.parser')
     images = soup.findAll('img')
@@ -111,8 +127,8 @@ def get_char(char):
     image_url = image["src"]
     return render_template("character.html", char_name=char_name, char_gender=char_gender, char_culture=char_culture,
                            char_born=char_born, char_died=char_died, char_titles=char_titles, char_aliases=char_aliases,
-                           char_spouse=char_spouse, char_allegiance=char_allegiance_list, char_books=char_books_list,
-                           char_actor=char_actor, char_image=image_url)
+                           char_spouse=spouse_dict, char_allegiance=allegiance_dict, char_books=char_books_dict,
+                           char_actor=char_actor_string, char_image=image_url)
 
 
 if __name__ == '__main__':
